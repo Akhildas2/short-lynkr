d2513c015ad393ff314c2068b7c76044076199fc
Akhildas2 <akhildasaki12@gmail.com>
2025-04-29 21:24:08 +0530

    added content ,changed styles

--- client/src/app/features/common/home/home.component.ts
+++ client/src/app/features/common/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { MaterialModule } from '../../../../Material.Module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,MaterialModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
