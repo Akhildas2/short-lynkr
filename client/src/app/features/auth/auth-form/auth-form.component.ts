0f6d09ff4c143d289ec0272c67448f90bd7430bb
Akhildas2 <akhildasaki12@gmail.com>
2025-05-03 23:08:42 +0530

    added auth page and design it like login,register

--- client/src/app/features/auth/auth-form/auth-form.component.ts
+++ client/src/app/features/auth/auth-form/auth-form.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { MaterialModule } from '../../../../Material.Module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HeaderComponent,FooterComponent,MaterialModule,FormsModule],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.scss'
 })
 export class AuthFormComponent {
+  isActive = false;
 
+  toggleForm() {
+    this.isActive = !this.isActive;
+  }
 }
