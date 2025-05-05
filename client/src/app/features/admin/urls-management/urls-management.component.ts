66c1b5d94b6a96ccfad62c689a03af3d74aa4f83
Akhildas2 <akhildasaki12@gmail.com>
2025-05-05 22:29:40 +0530

    added pages

---
+++ client/src/app/features/admin/urls-management/urls-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-urls-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './urls-management.component.html',
+  styleUrl: './urls-management.component.scss'
+})
+export class UrlsManagementComponent {
+
+}
