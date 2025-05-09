26bd48750cd8432e7b49e1f87c1bdc77ef399770
Akhildas2 <akhildasaki12@gmail.com>
2025-05-09 23:14:07 +0530

    changed state to singal and upgrad cli

--- client/src/app/features/admin/users-management/users-management.component.ts
+++ client/src/app/features/admin/users-management/users-management.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-users-management',
-  standalone: true,
-  imports: [],
-  templateUrl: './users-management.component.html',
-  styleUrl: './users-management.component.scss'
+    selector: 'app-users-management',
+    imports: [],
+    templateUrl: './users-management.component.html',
+    styleUrl: './users-management.component.scss'
 })
 export class UsersManagementComponent {
 
--- client/src/app/state/auth/auth.effects.ts
+++ client/src/app/state/auth/auth.effects.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { AuthApiService } from "../../core/services/api/auth/auth-api.service";
 import { AuthStore } from "./auth.store";
 import { Router } from "@angular/router";
@@ -9,35 +9,63 @@ import { firstValueFrom } from "rxjs";
 @Injectable({ providedIn: 'root' })
 
 export class authEffects {
-    constructor(private authApiService: AuthApiService, private authStore: AuthStore, private router: Router,private snackbar:SnackbarService) { }
+    private authApiService = inject(AuthApiService);
+    private authStore = inject(AuthStore);
+    private router = inject(Router);
+    private snackbar = inject(SnackbarService);
 
-    async login(email: string, password: string):Promise<void> {
+    async login(email: string, password: string): Promise<void> {
+        this.authStore.setLoading();
+
         try {
             const response = await firstValueFrom(this.authApiService.login({ email, password }));
-            this.authStore.setUser(response.user);
-            localStorage.setItem('token', response.token);
-            this.snackbar.showSuccess(`Welcome back, ${response.user.username}!`);
+
+            this.authStore.setAuthData(response.user, response.token);
+            this.snackbar.showSuccess(`Welcome back,${response.user.username || 'User'}!`);
+
             this.router.navigate(['']);
-        } catch (error:any) {
-            this.snackbar.showError(error?.error?.message || 'Login failed. Please check your credentials.');
+
+        } catch (error: any) {
+            const errorMessage = error?.error?.message || 'Login failed. Please check your credentials.';
+
+            this.authStore.setError(errorMessage)
+            this.snackbar.showError(errorMessage);
         }
     }
 
-    async register(username: string, email: string, password: string):Promise<void> {
+    async register(username: string, email: string, password: string): Promise<void> {
+        this.authStore.setLoading();
         try {
             const response = await firstValueFrom(this.authApiService.register({ username, email, password }));
-            this.authStore.setUser(response.user);
-            localStorage.setItem('token', response.token);
-            this.snackbar.showSuccess(`Account created successfully! Welcome, ${response.user.username}.`);
+
+            this.authStore.setAuthData(response.user, response.token);
+            this.snackbar.showSuccess(`Account created successfully! Welcome, ${response.user.username || 'User'}.`);
             this.router.navigate([''])
-        } catch (error:any) {
-            this.snackbar.showError(error?.error?.message || 'Registration failed. Please try again.');
+        } catch (error: any) {
+            const errorMessage = error?.error?.message || 'Registration failed. Please try again.';
+            this.authStore.setError(errorMessage);
+            this.snackbar.showError(errorMessage);
+        }
+    }
+
+    async checkAuthStatus(): Promise<void> {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            this.authStore.clearAuth();
+            return;
+        }
+
+        try {
+            const response = await firstValueFrom(this.authApiService.getProfile());
+            this.authStore.setUser(response.user);
+
+        } catch (error) {
+            this.logout();
         }
     }
 
     logout() {
-        this.authStore.clearUser();
-        localStorage.removeItem('token');
+        this.authStore.clearAuth();
         this.snackbar.showInfo('You have been logged out.');
         this.router.navigate(['/auth/login'])
     }
