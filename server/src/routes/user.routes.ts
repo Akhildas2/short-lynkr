4e943c14b6456fb29a339373f932792af9c40611
Akhildas2 <akhildasaki12@gmail.com>
2025-05-18 22:40:16 +0530

    updated and formated

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -5,9 +5,9 @@ import { authenticate } from "../middleware/auth";
 
 const router = Router();
 
-router.post('/register',register);
-router.post('/login',login)
-router.get('/me',authenticate,getProfile)
+router.post('/register', register);
+router.post('/login', login)
+router.get('/me', authenticate, getProfile)
 
 
 export default router;
