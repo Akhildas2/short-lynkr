c421e4b5f567b686c7a3785a1c29bd18d542dd20
Akhildas2 <akhildasaki12@gmail.com>
2025-06-25 22:41:14 +0530

    updated code of profile,analytics,header etc...

--- client/src/app/features/user/profile/profile.component.ts
+++ client/src/app/features/user/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
     selector: 'app-profile',
-    imports: [],
+    imports: [HeaderComponent, FooterComponent],
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.scss'
 })
