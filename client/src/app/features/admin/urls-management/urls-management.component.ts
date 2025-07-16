c1b2177735f90aded4069ae101ac8386bf33b29d
Akhildas2 <akhildasaki12@gmail.com>
2025-07-16 22:26:02 +0530

    added urls-management and fixed design in alert,user-management

--- client/src/app/features/admin/urls-management/urls-management.component.ts
+++ client/src/app/features/admin/urls-management/urls-management.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { AdminEffects } from '../../../state/admin/admin.effects';
+import { AdminStore } from '../../../state/admin/admin.store';
+import { effect } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 @Component({
     selector: 'app-urls-management',
-    imports: [],
+    imports: [SharedModule],
     templateUrl: './urls-management.component.html',
     styleUrl: './urls-management.component.scss'
 })
-export class UrlsManagementComponent {
+export class UrlsManagementComponent implements OnInit{
+    private adminStore = inject(AdminStore);
+    private adminEffect = inject(AdminEffects);
+
+    urls=computed(()=>this.adminStore.urls());
+    async ngOnInit(): Promise<void> {
+       await this.adminEffect.fetchAllUrls();
+    }
+
+
 
 }
