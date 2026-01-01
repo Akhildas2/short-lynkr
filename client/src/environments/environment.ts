c49942132abc7b33cd9f557f9003224fa9a7ceb4
Akhildas2 <akhildasaki12@gmail.com>
2026-01-01 12:26:55 +0530

    added env for frontend hosting

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: false,
+    production: true,
 
-    baseApiUrl: 'http://localhost:3333',
-    apiUrl: 'http://localhost:3333/api',
-    urlApiUrl: 'http://localhost:3333/api/url',
-    authApiUrl: 'http://localhost:3333/api/auth',
-    userApiUrl: 'http://localhost:3333/api/user',
-    adminApiUrl: 'http://localhost:3333/api/admin',
+    baseApiUrl: 'https://short-lynkr-backend.onrender.com',
+    apiUrl: 'https://short-lynkr-backend.onrender.com/api',
+    urlApiUrl: 'https://short-lynkr-backend.onrender.com/api/url',
+    authApiUrl: 'https://short-lynkr-backend.onrender.com/api/auth',
+    userApiUrl: 'https://short-lynkr-backend.onrender.com/api/user',
+    adminApiUrl: 'https://short-lynkr-backend.onrender.com/api/admin',
 
     GOOGLE_CLIENT_ID: '1029405622007-f74l4g9gvaihd14k6cp32lgobnjd1fa4.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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'
