3f08a0e777e23006ca375aaf6531d715cb7fdf94
Akhildas2 <akhildasaki12@gmail.com>
2026-01-02 09:47:45 +0530

    Refactor email transporter configuration for improved clarity and remove unnecessary console error logging

--- server/src/utils/email.utils.ts
+++ server/src/utils/email.utils.ts
@@ -6,8 +6,8 @@ import SettingsModel from '../models/settings.model';
 // Create a transporter object using Gmail SMTP
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587, // Changed from 465
-    secure: false, 
+    port: 587, 
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, 
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
     connectionTimeout: 10000,
     tls: {
         ciphers: 'SSLv3',
-        rejectUnauthorized: false // Use cautiously
+        rejectUnauthorized: false
     }
 });
 
@@ -41,8 +41,7 @@ export const sendEmail = async (options: EmailOptions) => {
 
         console.log('Message sent: %s', info.messageId);
     } catch (error: any) {
-        console.error('EMAIL ERROR 👉', error);
-        throw new ApiError(
+         throw new ApiError(
             error?.message || 'Error sending email',
             500
         );
